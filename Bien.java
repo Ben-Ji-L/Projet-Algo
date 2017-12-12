c508c2baf819de1b75766893a70ec4102cd7eb2e
Benjamin Lemaire <benjamin.lemaire59850@gmail.com>
2017-12-12 16:45:08 +0100

    Création du dépot et du prototype de la classe Bien.

---
+++ Bien.java
@@ -0,0 +1,53 @@
+/*
+    Cette classe représente les biens immobiliers
+    
+    String type le type du bien (studio, appartement...)
+    int numRue le numero de la rue
+    String rue le nom de la rue
+    int cp le code postal
+    String ville le nom de la ville
+*/
+class Bien {
+    
+    Biens (String type, int numRue, String rue, int cp, String ville) {
+        
+        
+        
+    }
+    
+    /*
+        Modifier le bien
+        
+        return le bien modifié
+        
+        int idBien l'identifiant unique du bien
+    */
+    Bien setBien (int idBien) {
+        
+        
+        
+    }
+    
+    /*
+        Supprimme le bien
+      
+        int idBien l'identifiant unique du bien
+    */
+    void delBien (int idBien) {
+        
+        
+        
+    }
+    
+    /*
+        Affiche les biens selon le type demandé par l'utilisateur
+        
+        return String les biens demandés
+        
+        String typeDemande le type de biens demandé
+    */
+    String affBiensParType (String typeDemande) {
+        
+        
+    }
+}
