160d5d3f0b3f682e0d3da5b67462fd53ea7ec405
Benjamin Lemaire <benjamin.lemaire59850@gmail.com>
2017-12-13 15:08:59 +0100

    Création de la classe des locataires et du menu

---
+++ Locataire.java
@@ -0,0 +1,30 @@
+/*
+    La classe qui représente les locataires
+*/
+class Locataire {
+    
+    Locataire () {
+        
+        
+        
+    }
+    
+    /*
+        Permet de modifier le locataire
+    */
+    void setLocataire () {
+        
+        
+        
+    }
+    
+    /*
+        supprime le locataire
+    */
+    void delLocataire () {
+        
+        
+        
+    }
+    
+}
---
+++ Menu.java
@@ -0,0 +1,57 @@
+class Menu {
+    
+    /*
+        Affiche les biens selon le type demandé par l'utilisateur
+        
+        return String les biens demandés
+        
+        String typeDemande le type de biens demandé
+    */
+    String affBiensParType (String typeDemande) {
+        
+        
+        
+    }
+    
+    /*
+        Affiche les locataires par ordre alphabétique
+        
+        return String la liste des locataires
+        
+        int[] idLoc l'identifiant du locataire
+    */
+    String affLocAlpha (int[] idLoc) {
+        
+        
+        
+    }
+    
+    /*
+        Affiche les locataires par type de biens
+        
+        return String la liste des locataires
+        
+        String[] type la liste des type de biens
+        String[]  nomLocataires la liste des noms de locataires
+    */
+    String affLocParTyBien (String[] type, String[] nomLocataires) {
+        
+        
+        
+    }
+    
+    /*
+        Affiche les locations par locataire
+        
+        return String la liste des locations
+        
+        String[]  nomLocations la liste des locations
+        String[]  nomLocataires la liste des noms de locataires
+    */
+    String rechLocationsParLocataire (String[] nomLocations, String[] nomLocataires) {
+        
+        
+        
+    }
+    
+}
