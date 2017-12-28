41beef24719f6802692aea8c872973f7a1ef942e
Margot Scrève <margot.colpart@mac.com>
2017-12-28 14:45:03 +0100

    Création des classes "menuGénéral" et "TypeDeBien".

---
+++ TypeDeBien.java
@@ -0,0 +1,30 @@
+/* 
+classe qui définit les types de biens et les actions possibles sur ces types 
+*/
+
+class TypeDeBien {
+    
+    TypeDeBien (int iDTypeBien, String nomTypeBien ){
+        
+    }
+    
+    //ajouter un type de bien
+    void ajouterType () {
+        
+    }
+    
+    //modifier un type de bien
+    void modifierType () {
+        
+    }
+    
+    //supprimer un type de bien
+    void supprimerType () {
+        
+    }
+    
+    //afficher la liste des types de biens
+    void afficherListeType () {
+        
+    }
+}
