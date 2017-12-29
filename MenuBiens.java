27df2299f0e77ea4bb3c26d7f385de1e4b045c07
benjamin <benjamin.lemaire59850@gmail.com>
2017-12-29 18:57:23 +0100

    Ebauche du menu des biens immobiliers.

---
+++ MenuBiens.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class MenuBiens {
+
+    void afficherMeu () {
+
+        Scanner sc = new ScScanner(System.in);
+        int numChoisi;
+
+        do {
+            System.out.print("\n__________MENU DES BIENS__________\n\n" +
+                    "[1] Ajouter un bien\n" +
+                    "[2] Modifier un bien\n" +
+                    "[3] Supprimer un bien\n" +
+                    "[4] Sortir du menu\n\n");
+
+            System.out.print("Sélectionner l'action souhaitée : ");
+            numChoisi = sc.nextInt();
+
+            switch (numChoisi) {
+                case 1:
+                    break;
+            }
+        } while(numChoisi!=4);
+
+    }
+
+}
