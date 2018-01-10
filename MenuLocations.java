c487854301c5f94f991aba11bf8af6da8b046ec4
Benjamin Lemaire <benjamin.lemaire59850@gmail.com>
2018-01-10 17:01:55 +0100

    La classe MenuLocations est commentée.

--- MenuLocations.java
+++ MenuLocations.java
@@ -1,9 +1,9 @@
-/**
- * Classe de menu gérant toutes les options de location
- */
 import java.io.IOException;
 import java.util.Scanner;
 
+/**
+ * Classe de menu gérant toutes les options de location
+ */
 public class MenuLocations {
     Scanner sc = new Scanner(System.in);
     int choixMenuLocations;
@@ -16,11 +16,12 @@ public class MenuLocations {
     void afficherMenu () throws IOException {
 
         do {
+        	// Le menu principal des locations
             System.out.print("\n______MENU DES LOCATIONS______\n\n" +
                     "[1] Louer un bien\n" +
                     "[2] Libérer un bien\n" +
                     "[3] Afficher la liste des biens loués\n" +
-                    "[4] Afficher la liste des locataires de biens\n" +
+                    "[4] Afficher la liste des locataires\n" +
                     "[5] Afficher la liste des locataires ayant au moins un bien en cours de location\n" +
                     "[6] Sortir du menu\n\n");
 
@@ -28,8 +29,12 @@ public class MenuLocations {
             System.out.print("Entrez le numéro de l'action souhaitée : ");
             choixMenuLocations = sc.nextInt();
 
+            /**
+             * Sélectionne l'action à effectuer selon le choix de l'utilisateur
+             */
             switch (choixMenuLocations) {
-
+            
+            	// Louer un bien
                 case 1:
                     if (listeDeBiens.getNbBiens() == 0) {
                         System.out.println("Aucun bien enregistré !");
@@ -42,6 +47,8 @@ public class MenuLocations {
                     if (listeLocataires.getNbLocataires() == 0) {
                     	System.out.println("Aucun locataire enregistré !");
                     	break;
+                    } else {
+                    	listeDeBiens.afficherLaListeDesBiensLoues();
                     }
                     listeLocataires.afficherListeSimplifiee();
                     System.out.print("Entrez l'identifiant du locataire : ");
@@ -56,6 +63,7 @@ public class MenuLocations {
                     System.out.println("Le bien est maintenant loué !");
                     break;
 
+                // Libérer un bien
                 case 2:
                 	if (listeDeBiens.getNbBiens() == 0) {
                 		System.out.println("Aucun bien enregistré !");
@@ -81,23 +89,29 @@ public class MenuLocations {
                     System.out.println("Le bien est maintenant libéré !");
                     break;
 
+                // Affiche la liste des biens loués
                 case 3:
-                	listeDeBiens.afficherLaListeDesBiensLoues();
                     System.out.print ("La liste des biens actuellement loués est la suivante : ");
                     listeDeBiens.afficherLaListeDesBiensLoues();
                     break;
 
+                // Affiche la liste des locataires
                 case 4:
-                	listeLocataires.afficherLocatairesLouantAuMoinsUnBien(true);
+                	if (listeLocataires.getNbLocataires() != 0) {
+                		listeLocataires.afficherLocatairesLouantAuMoinsUnBien(true);
+                	} else {
+                		System.out.println("Il n'y a aucun locataire enregistré !");
+                	}
                     break;
-
+                // Affiche la liste des locataires ayant au moins un bien loué
                 case 5:
                 	listeLocataires.afficherLocatairesLouantAuMoinsUnBien(false);
                     break;
-
+                // Permet de quitter le menu
                 case 6:
                     break;
 
+                // En cas de commande incorrecte
                 default:
                     System.out.print ("Commande incorrecte.");
 
